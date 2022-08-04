cb1c284cf84096b7154718e279b81e08f6cc6cab
FNFIS\e3005080 <salam.abdus@fisglobal.com>
2022-08-05 00:14:41 +0530

    java file

---
+++ src/main/java/com/ubsquestion/UBSTest.java
@@ -0,0 +1,16 @@
+package com.ubsquestion;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class UBSTest
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World");
+   }
+
+}
